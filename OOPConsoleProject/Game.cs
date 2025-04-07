7582e3f814e5fb8fbddff47958a3bafeb3a68805
JungDaWit <ekdnlt10@gmail.com>
2025-04-07 11:27:03 +0900

    [Feat] 게임 씬 동작 프레임 워크 제작
    게임 클래스를 구현하고 여러 신들을 가질 수 있도록 구현하여, 앞으로 많은 씬들을 추가하며 동작할 수 있는 구성을 만듬

---
+++ OOPConsoleProject/Game.cs
@@ -0,0 +1,50 @@
+﻿using OOPConsoleProject.Scene;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    public static class Game
+    {
+
+        private static Dictionary<string, BaseScene> sceneDic;
+        private static BaseScene curScene;
+
+        private static bool gameOver;
+        public static void Run()
+        {
+            while (gameOver == false)
+            {
+                curScene.Rende();
+                curScene.Input();
+                curScene.Update();
+                curScene.Result();
+            }
+        }
+        //<summary>
+        //게임의 초기 설정들 진행
+        //</summary>
+        private static void Start()
+        {
+            //게임 설정
+            gameOver = false;
+
+            //씬 설정
+            sceneDic = new Dictionary<string, BaseScene>();
+            sceneDic.Add("Title", new TitleScene());
+
+            curScene = sceneDic["Title"];
+        }
+        //<summary>
+        //게임의 마무리 작업 진행
+        //</sumary>
+        private static void End()
+        {
+
+        }
+
+    }
+}
---
+++ OOPConsoleProject/Scene/TitleScene.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject.Scene
+{
+    public class TitleScene : BaseScene
+    {
+        public override void Input()
+        {
+            Console.ReadKey();
+        }
+
+        public override void Rende()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Result()
+        {
+            Console.WriteLine("********************");
+            Console.WriteLine("       레전드 RPG     ");
+            Console.WriteLine("********************");
+            Console.WriteLine();
+            Console.WriteLine("계속하려면 아무키나 누르세요");
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
